e004f9fb00e0e15aa999dce556cede6f64d08ff7
AdministradorCIFO <albertprofe@gmail.com>
2022-06-22 11:12:33 +0200

    commit #3 : upload Image draft

--- library7/src/main/java/io/company/library/controller/BookImagesController.java
+++ library7/src/main/java/io/company/library/controller/BookImagesController.java
@@ -13,10 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 import java.util.Base64.Encoder;
-
 import java.util.Base64;
 import java.util.Optional;
 
