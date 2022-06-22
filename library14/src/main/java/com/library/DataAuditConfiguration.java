501ac4a80976a15d21aa0e8c1ee3919efebe0f76
Albert.Profe <albertprofe@gmail.com>
2022-06-22 16:41:57 +0200

    Commit #2 : version 2.0  evolution wiht audit, configuration, builder commandlinerunner and others

---
+++ library14/src/main/java/com/library/DataAuditConfiguration.java
@@ -0,0 +1,17 @@
+package com.library;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.ReactiveAuditorAware;
+import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
+import reactor.core.publisher.Mono;
+
+@Configuration
+@EnableReactiveMongoAuditing
+class DataAuditConfiguration{
+
+    @Bean
+    ReactiveAuditorAware<String> auditorAware() {
+        return () -> Mono.just("Albert Profe CIFO JAVA 2022");
+    }
+}
--- library14/src/main/java/com/library/controller/BookRestController.java
+++ library14/src/main/java/com/library/controller/BookRestController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 
 @RestController
@@ -27,9 +28,9 @@ public class BookRestController {
     }
 
     @PostMapping("/save")
-    public void saveBook(@RequestBody Book book){
-        bookRepository.save(book).subscribe();
-       System.out.println("executed" + book);
+    public Mono<Book> saveBook(@RequestBody final Book book){
+       //System.out.println("executed" + book);
+       return  bookRepository.save(book);
     }
 
 
